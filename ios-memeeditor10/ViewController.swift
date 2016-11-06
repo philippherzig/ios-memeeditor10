0a36c839df4327a6fe327dc7001de421d2793d85
Philipp Herzig <phil@Philipps-Mac-mini.local>
2016-11-06 16:26:45 +0100

    imagePickerController is accessible and can be dismissed

--- ios-memeeditor10/ViewController.swift
+++ ios-memeeditor10/ViewController.swift
@@ -12,14 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickImage(_ sender: UIBarButtonItem) {
+        let destinationController = UIImagePickerController()
+        present(destinationController, animated: true, completion: nil)
     }
-
+    
+    @IBAction func makeImage(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func shareImage(_ sender: UIBarButtonItem) {
+    }
 
 }
 
